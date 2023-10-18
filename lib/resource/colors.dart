21011256f3cd7e8566c6aec75f0f02ccd10c60ef
MrNIMA87 <nimashamsi1400@gmail.com>
2023-10-18 07:40:10 +0330

    add Project infrastructure

---
+++ lib/resource/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color title = Colors.black;
+  static const Color hint = Color.fromARGB(255, 217, 220, 228);
+  static const Color appBar = Color.fromARGB(255, 255, 255, 255);
+  static const Color borderColor = Color.fromARGB(255, 217, 220, 228);
+  static const Color focusedBorderColor = Color.fromARGB(255, 0, 117, 251);
+  static const Color buttonColor = Color.fromARGB(255, 0, 117, 251);
+  static const Color suraFaceColor = Color.fromARGB(255, 243, 243, 243);
+}
